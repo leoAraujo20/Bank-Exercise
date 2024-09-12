ff24a7f9b76ee6eefaebcb749b3f6d3d2c919c65
Leônidas Araújo <leonidasaraujo19@gmail.com>
2024-09-12 17:22:40 -0300

    Update bank_classes.py

--- bank_classes.py
+++ bank_classes.py
@@ -1,6 +1,26 @@
 from abc import ABC, abstractmethod
 
-class Pessoa():
+class Account(ABC):
+    
+    def __init__ (self, agency, account_number, balance):
+        self.agency = agency
+        self.account_number = account_number
+        self.balance = balance
+    
+    def deposit_money(self,value):
+        self.balance += value
+
+    @abstractmethod
+    def withdraw_money():
+        raise NotImplementedError('Implement this class according to the type of bank')
+
+class CheckingAccount(Account):
+
+    def withdraw_money(self, value):
+        self.balance -= value
+        
+
+class Person():
     
     def __init__(self, name, age):
         self._name = name
@@ -13,12 +33,16 @@ class Pessoa():
     @name.setter
     def name(self,name):
         self._name = name
+    
+class Client(Person):
+    pass
 
 if __name__ == '__main__':
-    person1 = Pessoa('João',25)
+    person1 = Person('João',25)
     print(person1.name)
     person1.name = 'Felipe'
     print(person1.name)
+
     
 
         
